38df54239f56aad7dbbfac3c0d2ddbafe4658eab
hosodakazufumi <hosodakazufumi@gmail.com>
2022-06-02 20:35:32 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fp:
+  long_description = fp.read()
+
+setuptools.setup(
+  name="tfdone",
+  version="0.0.1",
+  author="Kazufumi Hosoda",
+  author_email="hosodakazufumi@gmail.com",
+  description="TensorFlow implementation of DONE",
+  long_description="TensorFlow implementation of DONE (Direct ONE-shot learning with Hebbian weight imprinting)",
+  long_description_content_type="text/markdown",
+  url="https://github.com/hosodakazufumi/tfdone",
+  packages=find_packages(),
+  license="Apache License 2.0",
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+  ],
+)
